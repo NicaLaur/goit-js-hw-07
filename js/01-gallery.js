0b8cc0db1f3b73a2578bed498c59f93c776594e3
NicaLaur <142085072+NicaLaur@users.noreply.github.com>
2024-01-18 14:34:56 +0200

    Task-1

---
+++ js/01-gallery.js
@@ -0,0 +1,52 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const ulGallery = document.querySelector('.gallery');
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+galleryItems.forEach(element => {
+
+    const liGallery = document.createElement('li')
+liGallery.classList.add('gallery__item')
+ulGallery.appendChild(liGallery)
+
+const linkGallery = document.createElement('a')
+linkGallery.classList.add('gallery__link')
+liGallery.appendChild(linkGallery)
+    linkGallery.setAttribute('href', element.preview)
+    
+    const imgGallery = document.createElement('img')
+    imgGallery.classList.add('gallery__image')
+    imgGallery.setAttribute('src', element.preview)
+    imgGallery.setAttribute('data-source', element.original)
+    imgGallery.setAttribute('alt', element.description)
+    linkGallery.appendChild(imgGallery)
+
+});
+    
+    const modal = basicLightbox.create('<img>')
+    const modalImg = modal.element().querySelector('img')
+    
+    ulGallery.addEventListener('click', function(event) {
+        event.preventDefault();
+    modalImg.src = event.target.dataset.source;
+    modal.show();
+
+    }, true)
+
+    document.addEventListener('keydown', evt => {
+        if (evt.key === 'Escape' && modal.visible()) {
+            modal.close()
+        }
+    })
+    
+})
+
+
+
+
+
+

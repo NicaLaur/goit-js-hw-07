66bda3134cf671b4f812d4ca192867fe506b0c69
NicaLaur <142085072+NicaLaur@users.noreply.github.com>
2024-01-21 14:36:04 +0200

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,32 +2,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
 
 const ulGallery = document.querySelector('.gallery');
 
-document.addEventListener('DOMContentLoaded', function () {
-
-    galleryItems.forEach(element => {
-
-        const liGallery = document.createElement('li')
-        liGallery.classList.add('gallery__item')
-        ulGallery.appendChild(liGallery)
-
-        const linkGallery = document.createElement('a')
-        linkGallery.classList.add('gallery__link')
-        liGallery.appendChild(linkGallery)
-        linkGallery.setAttribute('href', element.preview)
-
-        const imgGallery = document.createElement('img')
-        imgGallery.classList.add('gallery__image')
-        imgGallery.setAttribute('src', element.preview)
-        imgGallery.setAttribute('alt', element.description)
-        linkGallery.appendChild(imgGallery)
-
-    });
 
 
-})
+const ulChild = galleryItems
+    .map(({preview, original, description}) => `<li class="gallery__item">
+   <a class="gallery__link" href=${original}>
+      <img class="gallery__image" src=${preview} alt= ${description} />
+   </a>
+</li>`)
+    .join("");
+
+
+ulGallery.innerHTML = ulChild;
+
+
+
 
 
 new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 })
+
